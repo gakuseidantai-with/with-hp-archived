42b246d1b12ac7695a9922c07619fd36af26601f
gakuseidantai-with <m18kecam777@gmail.com>
2021-07-04 15:00:22 +0900

    ✨css module設定

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,7 @@
+import classes from 'src/components/Header/Header.module.css'
+
+const Header: React.FC = () => {
+  return <header></header>
+}
+
+export default Header
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,9 @@ import { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <>
-      <div>
-        <p>ここはwithのホームページです</p>
-      </div>
-    </>
+    <div>
+      <p>ここはwithのホームページです</p>
+    </div>
   )
 }
 
