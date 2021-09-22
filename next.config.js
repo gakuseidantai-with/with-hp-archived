15c691d5003285692d32851710616dd0630d80ca
h.nomura <>
2021-09-22 21:10:59 +0900

    base整備

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
-const path = require('path')
+/**
+ * Next Configuration
+ */
+/** @type {import('next/dist/next-server/server/config-shared').NextConfig} */
+const config = (_phase, { defaultConfig }) => {
+  return {
+    ...defaultConfig,
+    reactStrictMode: true,
+    eslint: {
+      dirs: ["src/pages"],
+    },
+  };
+};
 
-module.exports = {
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
-  reactStrictMode: true,
-}
+module.exports = config;
