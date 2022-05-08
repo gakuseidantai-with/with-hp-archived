8d6150371cf270eb5450b44c4aabfc59cf38d279
h.nomura <>
2022-05-09 02:47:12 +0900

    refactor

--- src/layout/MainLayout/MainLayout.tsx
+++ src/layout/MainLayout/MainLayout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Header } from "~/components/Header";
 import { css } from "linaria";
-import { Footer } from "~/components/footer";
+import { Footer } from "~/components/Footer";
 
 type Props = {
   children: React.ReactNode | React.ReactNodeArray;
