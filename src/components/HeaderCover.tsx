8264015c2209444be8dc5addc1a6e6586a8af14a
h.nomura <>
2022-05-15 14:51:48 +0900

    fix build error

--- src/components/HeaderCover.tsx
+++ src/components/HeaderCover.tsx
@@ -6,15 +6,21 @@ type Props = {
   imagePath: string;
 };
 
-export const HeaderCover: React.FC<Props> = React.memo(({ imagePath }): JSX.Element => {
-  // return <img src={imagePath} alt="with_logo" className={styles["headerCover"]} />;
-  return (
-    <>
-      <Script src="/js/animation.js" id="animation" />
-      <div id="visual" className={styles["headerCover"]}></div>
-    </>
-  );
-});
+export const HeaderCover: React.FC<Props> = React.memo(
+  (
+    {
+      /* imagePath */
+    }
+  ): JSX.Element => {
+    // return <img src={imagePath} alt="with_logo" className={styles["headerCover"]} />;
+    return (
+      <>
+        <Script src="/js/animation.js" id="animation" />
+        <div id="visual" className={styles["headerCover"]}></div>
+      </>
+    );
+  }
+);
 
 const styles = {
   headerCover: css`
